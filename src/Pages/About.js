dc2c53e1d343ded58fe2d00c47ff2fa1e04b6f1c
228miles <hangdothiduc@gmail.com>
2017-04-29 04:27:55 -0400

    update readme, fix link

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,7 +7,7 @@ class About extends Component {
     return (
       <div className="content">
         <p><i>Me And My Facebook Data</i> is a project by <a href="http://22-8miles.com" target="_blank">Hang Do Thi Duc</a>.</p>
-        <p>It was started in the <a href="http://webresidencies-solitude-zkm.com" target="_blank">web residency program</a> <a href="">Blowing the Whistle, Questioning Evidence</a> curated by Tatiana Bazichelli with Solitude and ZKM in March 2017.</p>
+        <p>It was started in the <a href="http://webresidencies-solitude-zkm.com" target="_blank">web residency program</a> <a href="https://schloss-post.com/category/web-residents/blowing-the-whistle/" target="_blank">Blowing the Whistle, Questioning Evidence</a> curated by Tatiana Bazichelli with Solitude and ZKM in March 2017.</p>
         <p>This is an ongoing <a href="https://schloss-post.com/category/me-and-my-facebook-data/" target="_blank">project</a>. Check back for possibly more analysis and visualizations of your facebook data (e.g. more details on what kind of links you are sharing, what topics you talk about).</p>
         <p>The code with details about what tech I used can be found at <a href="https://github.com/01fade/me-and-my-fb-data" target="_blank">my github repo</a> for the project.</p>
         <p>If you ran into problems or have a general comment, email me at <i>hang[at]22-8miles.com</i>.</p>
